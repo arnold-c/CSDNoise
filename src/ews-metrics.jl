04cabe82c142b198572fd6f0e6b13fe1e3483c09
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2024-03-22 16:12:41 -0400

    Start to calculate mean EWS

--- src/ews-metrics.jl
+++ src/ews-metrics.jl
@@ -4,12 +4,27 @@ function calculate_centered_mean()
 end
 
 function calculate_backward_mean(time_step, bandwidth, timeseries)
+    mean_vec = zeros(Float64, length(timeseries))
+
+    calculate_backward_mean!(mean_vec, time_step, bandwidth, timeseries)
+
+    return mean_vec
 end
 
 function calculate_backward_mean!(mean_vec, time_step, bandwidth, timeseries)
+    # TODO: implement
+    delta = (bandwidth - 1) * time_step
+    for i in eachindex(timeseries)
+        mean_vec[i] = calculate_mean(
+            @view(timeseries[(i - delta):(i + delta)])
+        )
+    end
+    return mean_vec
 end
 
 function calculate_mean(timeseries)
+    # TODO: implement
+    return nothing
 end
 
 function calculate_autocorrelation()

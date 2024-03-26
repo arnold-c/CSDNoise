abbd724e761154ed6eeaa784cc35e8fd2f4145be
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2024-03-26 16:56:56 -0400

    Add unit test for ews mean and correct

--- src/ews-metrics.jl
+++ src/ews-metrics.jl
@@ -1,9 +1,10 @@
 using Statistics
+using TestItems
 
 function calculate_centered_mean(timeseries, time_step, bandwidth)
     mean_vec = zeros(Float64, length(timeseries))
 
-    calculate_centered_mean!(mean_vec, time_step, bandwidth, timeseries)
+    calculate_centered_mean!(mean_vec, timeseries, time_step, bandwidth)
 
     return mean_vec
 end
@@ -28,10 +29,37 @@ function calculate_centered_mean!(mean_vec, timeseries, time_step, bandwidth)
     return mean_vec
 end
 
+@testitem "Centered mean" begin
+    using CSDNoise
+    using Statistics
+
+    daily_testpositives = collect(1:10)
+
+    centered_mean_testpositives = calculate_centered_mean(
+        daily_testpositives, 1, 3
+    )
+
+    @test isequal(
+        centered_mean_testpositives,
+        [
+            mean([1]),
+            mean([1, 2, 3]),
+            mean([1, 2, 3, 4, 5]),
+            mean([2, 3, 4, 5, 6]),
+            mean([3, 4, 5, 6, 7]),
+            mean([4, 5, 6, 7, 8]),
+            mean([5, 6, 7, 8, 9]),
+            mean([6, 7, 8, 9, 10]),
+            mean([8, 9, 10]),
+            mean([10]),
+        ],
+    )
+end
+
 function calculate_backward_mean(timeseries, time_step, bandwidth)
     mean_vec = zeros(Float64, length(timeseries))
 
-    calculate_backward_mean!(mean_vec, time_step, bandwidth, timeseries)
+    calculate_backward_mean!(mean_vec, timeseries, time_step, bandwidth)
 
     return mean_vec
 end
@@ -39,6 +67,33 @@ end
 function calculate_backward_mean!(mean_vec, timeseries, time_step, bandwidth)
     calculate_movingavg!(mean_vec, timeseries, bandwidth * time_step)
     return mean_vec
+end
+
+@testitem "Backward mean" begin
+    using CSDNoise
+    using Statistics
+
+    daily_testpositives = collect(1:10)
+
+    backward_mean_testpositives = calculate_backward_mean(
+        daily_testpositives, 1, 3
+    )
+
+    @test isequal(
+        backward_mean_testpositives,
+        [
+            mean([1]),
+            mean([1, 2]),
+            mean([1, 2, 3]),
+            mean([2, 3, 4]),
+            mean([3, 4, 5]),
+            mean([4, 5, 6]),
+            mean([5, 6, 7]),
+            mean([6, 7, 8]),
+            mean([7, 8, 9]),
+            mean([8, 9, 10]),
+        ],
+    )
 end
 
 function calculate_autocorrelation()

590a75cfa2a4285a900b48db40b40d3fe9d090d5
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2024-03-26 16:22:45 -0400

    Rough draft of centered and backward mean functions

--- src/ews-metrics.jl
+++ src/ews-metrics.jl
@@ -1,9 +1,34 @@
 using Statistics
 
-function calculate_centered_mean()
+function calculate_centered_mean(timeseries, time_step, bandwidth)
+    mean_vec = zeros(Float64, length(timeseries))
+
+    calculate_centered_mean!(mean_vec, time_step, bandwidth, timeseries)
+
+    return mean_vec
 end
 
-function calculate_backward_mean(time_step, bandwidth, timeseries)
+function calculate_centered_mean!(mean_vec, timeseries, time_step, bandwidth)
+    # TODO: implement
+    delta = (bandwidth - 1) * time_step
+    tlength = length(timeseries)
+    for i in eachindex(timeseries)
+        if i <= delta
+            mean_vec[i] = mean(@view(timeseries[begin:(2i - 1)]))
+            continue
+        end
+        if i > tlength - delta
+            mean_vec[i] = mean(@view(timeseries[(2i - tlength):end]))
+            continue
+        end
+        mean_vec[i] = mean(
+            @view(timeseries[(i - delta):(i + delta)])
+        )
+    end
+    return mean_vec
+end
+
+function calculate_backward_mean(timeseries, time_step, bandwidth)
     mean_vec = zeros(Float64, length(timeseries))
 
     calculate_backward_mean!(mean_vec, time_step, bandwidth, timeseries)
@@ -11,20 +36,9 @@ function calculate_backward_mean(time_step, bandwidth, timeseries)
     return mean_vec
 end
 
-function calculate_backward_mean!(mean_vec, time_step, bandwidth, timeseries)
-    # TODO: implement
-    delta = (bandwidth - 1) * time_step
-    for i in eachindex(timeseries)
-        mean_vec[i] = calculate_mean(
-            @view(timeseries[(i - delta):(i + delta)])
-        )
-    end
+function calculate_backward_mean!(mean_vec, timeseries, time_step, bandwidth)
+    calculate_movingavg!(mean_vec, timeseries, bandwidth * time_step)
     return mean_vec
-end
-
-function calculate_mean(timeseries)
-    # TODO: implement
-    return nothing
 end
 
 function calculate_autocorrelation()

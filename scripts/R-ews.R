f6c5c3257e7fc1c2b9c91b7955ca02eeaf1063d6
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2024-04-26 17:24:24 -0400

    Move R EWS code back to R script to source
    Use RCall to source the file - easier to write with LSP and linter

---
+++ scripts/R-ews.R
@@ -0,0 +1,20 @@
+library(spaero)
+library(tidyverse)
+
+inc_array <- read_csv(here::here("out", "incidence-array_sim-1.csv"))
+
+spaero_inc_ews_stats <- get_stats(
+  inc_array[, "incidence"],
+  center_bandwidth = 30,
+  stat_bandwidth = 30,
+  center_trend = "local_constant",
+  stat_trend = "local_constant",
+  center_kernel = "uniform",
+  stat_kernel = "uniform",
+  lag = 1,
+)$stats
+
+spaero_ensemble_single_inc_ews_arr <- spaero_inc_ews_stats %>%
+  as_tibble() %>%
+  mutate(time = row_number()) %>%
+  select(time, everything())
--- scripts/ensemble-sim_inferred-scenario-visualizations.jl
+++ scripts/ensemble-sim_inferred-scenario-visualizations.jl
@@ -219,23 +219,7 @@ CSV.write(
 
 #%%
 R"""
-library(spaero)
-library(tidyverse)
-
-inc_array <- read_csv(here::here("out", "incidence-array_sim-1.csv"))
-
-spaero_inc_ews_stats <- get_stats(inc_array[, "incidence"],
-  center_kernel = "uniform",
-  center_trend = "local_constant", center_bandwidth = 30,
-  stat_bandwidth = 30
-)
-
-spaero_ensemble_single_inc_ews_arr <-
-  spaero_inc_ews_stats$stats %>%
-  as_tibble() %>%
-  mutate(time = row_number()) %>%
-  select(time, everything())
-  # pivot_longer(cols = c(everything(), -time))
+source(here::here("scripts","R-ews.R"))
 """
 
 @rget spaero_ensemble_single_inc_ews_arr

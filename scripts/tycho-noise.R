1e6fd537a988cdbd35fa2caa3b5a18fb9aac282c
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2024-08-19 19:25:54 -0400

    Add testing to tycho data

--- scripts/tycho-noise.R
+++ scripts/tycho-noise.R
@@ -89,7 +89,7 @@ noise_df <- add_noise(tycho_CA_measles_wide_plotdata, average_incidence, aggrega
   fill_aggregate_cases()
 
 # %%
-noise_wide_df <- noise_df %>%
+noise_long_df <- noise_df %>%
   pivot_longer(
     cols = c(-date),
     names_to = c("type", "aggregation"),
@@ -98,10 +98,10 @@ noise_wide_df <- noise_df %>%
   ) %>%
   mutate(aggregation = factor(aggregation, levels = c("4wk", "2wk", "1wk")))
 
-noise_wide_df
+noise_long_df
 
 # %%
-noise_wide_df %>%
+noise_long_df %>%
   ggplot(
     aes(x = date, y = values, color = aggregation, fill = aggregation)
   ) +
@@ -113,3 +113,100 @@ noise_wide_df %>%
   scale_x_date(date_breaks = "1 years", date_labels = "%Y") +
   labs(x = "Date", y = "Incidence", color = "Aggregation", fill = "Aggregation", alpha = "Aggregation") +
   theme_minimal()
+
+# %%
+calculate_test_characteristics <- function(inc_noise_df, aggregation_weeks = 1, test_chars = list(prop_tested = 1.0, sensitivity = 1.0, specificity = 1.0)) {
+  cases_col <- paste0("cases_", aggregation_weeks, "wk")
+  noise_col <- paste0("noise_", aggregation_weeks, "wk")
+  total_tested_col <- paste0("total_tested_", aggregation_weeks, "wk")
+  test_pos_col <- paste0("test_pos_", aggregation_weeks, "wk")
+  test_neg_col <- paste0("test_neg_", aggregation_weeks, "wk")
+
+  cases_tested <- round(inc_noise_df[[cases_col]] * test_chars$prop_tested, 0)
+  noise_tested <- round(inc_noise_df[[noise_col]] * test_chars$prop_tested, 0)
+
+  true_positives <- round(cases_tested * test_chars$sensitivity, 0)
+  false_positive <- round(noise_tested * (1-test_chars$specificity), 0)
+
+  false_negatives <- cases_tested - true_positives
+  true_negatives <- noise_tested - false_positive
+
+  inc_noise_df[[total_tested_col]] <- cases_tested + noise_tested
+  inc_noise_df[[test_pos_col]] <- true_positives + false_positive
+  inc_noise_df[[test_neg_col]] <- true_negatives + false_negatives
+
+  return(inc_noise_df)
+}
+
+# %%
+perfect_test_chars <- list(prop_tested = 1.0, sensitivity = 1.0, specificity = 1.0)
+perfect_test_df <- calculate_test_characteristics(noise_df, aggregation_weeks = 1, perfect_test_chars) %>%
+  calculate_test_characteristics(aggregation_weeks = 2, perfect_test_chars) %>%
+  calculate_test_characteristics(aggregation_weeks = 4, perfect_test_chars) %>%
+  fill_aggregate_cases()
+
+# Quickly check things are being calculated as expected
+sum(perfect_test_df$test_pos_1wk == perfect_test_df$cases_1wk) == nrow(perfect_test_df)
+sum(perfect_test_df$test_neg_1wk == perfect_test_df$noise_1wk) == nrow(perfect_test_df)
+sum(perfect_test_df$total_tested_1wk == perfect_test_df$obs_1wk) == nrow(perfect_test_df)
+sum(perfect_test_df$test_pos_2wk == perfect_test_df$cases_2wk) == nrow(perfect_test_df)
+sum(perfect_test_df$test_neg_2wk == perfect_test_df$noise_2wk) == nrow(perfect_test_df)
+sum(perfect_test_df$total_tested_2wk == perfect_test_df$obs_2wk) == nrow(perfect_test_df)
+sum(perfect_test_df$test_pos_4wk == perfect_test_df$cases_4wk) == nrow(perfect_test_df)
+sum(perfect_test_df$test_neg_4wk == perfect_test_df$noise_4wk) == nrow(perfect_test_df)
+sum(perfect_test_df$total_tested_4wk == perfect_test_df$obs_4wk) == nrow(perfect_test_df)
+
+# %%
+perfect_test_long_df <- perfect_test_df %>%
+  pivot_longer(
+    cols = c(-date),
+    names_to = c("type", "aggregation"),
+    names_pattern = "(.+)_(.+)",
+    values_to = "values",
+  ) %>%
+  mutate(aggregation = factor(aggregation, levels = c("4wk", "2wk", "1wk")))
+
+# %%
+perfect_test_long_df %>%
+  ggplot(
+    aes(x = date, y = values, color = aggregation, fill = aggregation)
+  ) +
+  geom_line() +
+  scale_color_manual(values = plot_colors, aesthetics = c("color", "fill")) +
+  geom_area(aes(alpha = aggregation), position = "identity") +
+  facet_wrap(~type, scales = "free_y", ncol = 1) +
+  scale_alpha_manual(values = c(0.0, 0.1, 1.0)) +
+  scale_x_date(date_breaks = "1 years", date_labels = "%Y") +
+  labs(x = "Date", y = "Incidence", color = "Aggregation", fill = "Aggregation", alpha = "Aggregation") +
+  theme_minimal()
+
+
+# %%
+rdt_8080_test_chars <- list(prop_tested = 1.0, sensitivity = 0.8, specificity = 0.8)
+rdt_8080_test_df <- calculate_test_characteristics(noise_df, aggregation_weeks = 1, rdt_8080_test_chars) %>%
+  calculate_test_characteristics(aggregation_weeks = 2, rdt_8080_test_chars) %>%
+  calculate_test_characteristics(aggregation_weeks = 4, rdt_8080_test_chars) %>%
+  fill_aggregate_cases()
+
+# %%
+rdt_8080_test_long_df <- rdt_8080_test_df %>%
+  pivot_longer(
+    cols = c(-date),
+    names_to = c("type", "aggregation"),
+    names_pattern = "(.+)_(.+)",
+    values_to = "values",
+  ) %>%
+  mutate(aggregation = factor(aggregation, levels = c("4wk", "2wk", "1wk")))
+
+# %%
+rdt_8080_test_long_df %>%
+  filter(type %in% c("total_tested", "test_pos", "test_neg", "cases")) %>%
+  ggplot(
+    aes(x = date, y = values, color = type)
+  ) +
+  geom_line() +
+  scale_x_date(date_breaks = "1 years", date_labels = "%Y") +
+  facet_wrap(~aggregation, scales = "free_y", ncol = 1) +
+  labs(x = "Date", y = "Incidence", color = "Type") +
+  theme_minimal()
+

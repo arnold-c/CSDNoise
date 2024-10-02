371b8f7aa08249c7be2fb6d6f979fc23f4e6c97e
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2024-10-02 14:18:52 -0400

    Add 100% testing to ensemble simulations
    Will be the same as EWS for incidence, but include as easier to test out
    implementation of EWS to simulations than on a different data source

--- scripts/ensemble-sim.jl
+++ scripts/ensemble-sim.jl
@@ -116,10 +116,10 @@ noise_spec_vec = vcat(poisson_noise_spec_vec, dynamical_noise_spec_vec)
 # alertthreshold_vec = collect(10:4:50)
 alertthreshold_vec = [10]
 moveavglag_vec = [7]
-perc_clinic_vec = [0.6]
+perc_clinic_vec = [1.0, 0.6]
 # perc_clinic_test_vec = [collect(0.1:0.1:0.6)..., 1.0]
-perc_clinic_test_vec = [0.2]
-alert_method_vec = ["inferred_movingavg"]
+perc_clinic_test_vec = [1.0, 0.2]
+alert_method_vec = ["movingavg", "inferred_movingavg"]
 
 outbreak_detection_spec_vec = create_combinations_vec(
     OutbreakDetectionSpecification,

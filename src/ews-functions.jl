255bb956809c3a226485fc4c52f48a59baca0d1f
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2024-10-10 08:19:20 -0400

    Update simulation_tau_heatmap_df! to output ews_metric_specification
    Required for later filtering

--- src/ews-functions.jl
+++ src/ews-functions.jl
@@ -527,12 +527,14 @@ function simulation_tau_heatmap_df!(
     test_ewsmetrics,
     ews_metric;
     individual_test_specification = IndividualTestSpecification(1.0, 1.0, 0),
+    ews_metric_specification = EWSMetricSpecification(Centered, 7, 52, 1),
     ews_enddate_type = Reff_start::EWSEndDateType,
     statistic_function = StatsBase.mean,
 )
     @assert names(ews_df) == [
         "ews_metric",
         "test_specification",
+        "ews_metric_specification",
         "ews_enddate_type",
         "ews_metric_value",
         "ews_metric_vector",
@@ -550,6 +552,7 @@ function simulation_tau_heatmap_df!(
         (
             ews_metric,
             individual_test_specification,
+            ews_metric_specification,
             ews_enddate_type,
             ews_tau,
             getproperty(test_ewsmetrics, ews_metric_tau_sym),

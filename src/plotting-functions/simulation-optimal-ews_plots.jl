d6197bbc5aa7b3407272559a393eb54d4900922a
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2024-11-02 01:44:47 -0400

    Add survival curve plots

--- src/plotting-functions/simulation-optimal-ews_plots.jl
+++ src/plotting-functions/simulation-optimal-ews_plots.jl
@@ -29,9 +29,7 @@ function create_optimal_ews_plots(
         "variance",
     ],
     force = false,
-    base_plotpath = joinpath(
-        plotsdir(), "ensemble", "ews-hyperparam-optimization"
-    ),
+    base_plotpath = joinpath(plotsdir(), "ensemble"),
 )
     grouped_optimal_ews_df = DataFrames.groupby(
         optimal_ews_df,
@@ -58,28 +56,55 @@ function create_optimal_ews_plots(
             length(ews_metrics) *
                 length(unique(optimal_ews_df.test_specification))
 
-        noise_specification = optimal_heatmap_df.noise_specification[1]
-        noise_plotdir = getdirpath(noise_specification)
-        percent_tested = optimal_ews_df[1, :percent_tested]
-        noise_plotpath = joinpath(
+        burnin_time = ensemble_specification.time_parameters.burnin
+        @unpack min_burnin_vaccination_coverage,
+        max_burnin_vaccination_coverage,
+        min_vaccination_coverage,
+        max_vaccination_coverage =
+            ensemble_specification.dynamics_parameter_specification
+
+        ensemble_vax_plotpath = joinpath(
             base_plotpath,
-            noise_plotdir,
-            "percent-tested-$(percent_tested)",
+            "burnin-time-$(burnin_time)",
+            "min-burnin-vax_$(min_burnin_vaccination_coverage)",
+            "max-burnin-vax_$(max_burnin_vaccination_coverage)",
+            "min-vax_$(min_vaccination_coverage)",
+            "max-vax_$(max_vaccination_coverage)",
         )
-        mkpath(noise_plotpath)
+
+        percent_tested = optimal_ews_df[1, :percent_tested]
+        noise_specification = optimal_heatmap_df.noise_specification[1]
+        noise_dirpath = getdirpath(noise_specification)
+
+        noise_plotdir = joinpath(
+            ensemble_vax_plotpath,
+            noise_dirpath,
+            "percent-tested_$(percent_tested)",
+            # "sens-$(test_specification.sensitivity)_spec-$(test_specification.specificity)_lag-$(test_specification.test_result_lag)",
+        )
+        ews_metric_specification = optimal_heatmap_df.ews_metric_specification[1]
+        ews_enddate_type = optimal_heatmap_df.ews_enddate_type[1]
+        ews_enddate_type_str = split(string(ews_enddate_type), "::")[1]
+        ews_threshold_burnin = optimal_heatmap_df.ews_threshold_burnin[1]
+
+        ews_plotdir = joinpath(
+            noise_plotdir,
+            "optimal-heatmaps",
+            ews_metric_specification.dirpath,
+            ews_enddate_type_str,
+        )
+        mkpath(ews_plotdir)
 
         noise_descripton = get_noise_magnitude_description(noise_specification)
 
         plotpath = joinpath(
-            noise_plotpath,
+            ews_plotdir,
             "ews-heatmap_tiebreaker-$(tiebreaker_preference).png",
         )
 
         if !isfile(plotpath) || force
-            ews_metric_specification = optimal_heatmap_df.ews_metric_specification[1]
             @unpack method, aggregation, bandwidth, lag =
                 ews_metric_specification
-            ews_enddate_type = optimal_heatmap_df.ews_enddate_type[1]
 
             heatmap_subtitle =
                 "Noise: $(noise_descripton), Percent Tested: $(percent_tested), $(ews_enddate_type)" *
@@ -96,36 +121,47 @@ function create_optimal_ews_plots(
             )
         end
 
-        # test_description = get_test_description(individual_test_specification)
-        #
-        # survival_plotdir = joinpath(
-        #     noise_plotdir,
-        #     "survival",
-        # )
-        #
-        # for ews_metric in ews_metrics
-        #     plotpath = joinpath(
-        #         survival_plotdir,
-        #         "ews_survival_$(ews_metric)_ews_agg-$(aggregation).png",
-        #     )
-        #     if !isfile(plotpath) || force
-        #         survival_plot = simulate_and_plot_ews_survival(
-        #             subset_df,
-        #             ews_metric_specification,
-        #             ews_threshold_burnin,
-        #             ensemble_specification,
-        #             individual_test_specification,
-        #             ensemble_single_incarr,
-        #             null_single_incarr,
-        #             ensemble_single_Reff_thresholds_vec;
-        #             ews_metric = ews_metric,
-        #         )
-        #         save(
-        #             plotpath,
-        #             survival_plot,
-        #         )
-        #     end
-        # end
+        for test_specification in unique(optimal_heatmap_df.test_specification)
+            test_description = get_test_description(test_specification)
+
+            test_plotdir = joinpath(
+                noise_plotdir,
+                "sens-$(test_specification.sensitivity)_spec-$(test_specification.specificity)_lag-$(test_specification.test_result_lag)",
+            )
+
+            survival_plotdir = joinpath(
+                test_plotdir,
+                "survival",
+                ews_metric_specification.dirpath,
+                ews_enddate_type_str,
+                "tiebreaker-$(tiebreaker_preference)",
+            )
+            mkpath(survival_plotdir)
+
+            for ews_metric in ews_metrics
+                plotpath = joinpath(
+                    survival_plotdir,
+                    "ews_survival_$(ews_metric).png",
+                )
+                if !isfile(plotpath) || force
+                    survival_plot = simulate_and_plot_ews_survival(
+                        optimal_heatmap_df,
+                        ews_metric_specification,
+                        ews_threshold_burnin,
+                        ensemble_specification,
+                        test_specification,
+                        ensemble_single_incarr,
+                        null_single_incarr,
+                        ensemble_single_Reff_thresholds_vec;
+                        ews_metric = ews_metric,
+                    )
+                    save(
+                        plotpath,
+                        survival_plot,
+                    )
+                end
+            end
+        end
     end
     return nothing
 end
